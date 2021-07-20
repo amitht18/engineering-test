fd10d2b3d9fbac492ad97d3b92b69e9df32447fa
Amith Tiwari <amitht18@gmail.com>
2021-07-20 11:18:17 +0530

    Type Type Type 😵‍💫😵‍💫😵‍💫

--- front-end/src/shared/helpers/debounce.ts
+++ front-end/src/shared/helpers/debounce.ts
@@ -1,5 +1,5 @@
 export function debounce(func: any, time: number) {
-    let timer: NodeJS.Timeout;
+    let timer: any;
     return () => {
         clearTimeout(timer);
         timer = setTimeout(() => { func() }, time)
